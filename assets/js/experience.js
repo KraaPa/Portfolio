4fc5e7bced551481782071dadc548d2423932a02
KraaPaHub <scottyqsnt@gmail.com>
2022-03-09 12:05:11 +0100

    Initial Commit

---
+++ assets/js/experience.js
@@ -0,0 +1,91 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Stage Technicien de Support",
+    cardImage: "assets/images/experience-page/Atlantic.png",
+    place: "Meyzieu - ACTA",
+    time: "(2021)",
+    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+  },
+  {
+    title: "Stage Web Designer + Web Master",
+    cardImage: "assets/images/experience-page/TaiShiFood.jpg",
+    place: "Janneyrias -  Tai Shi Food",
+    time: "(10 Janvier 2022 - 18 Février 2022)",
+    desp: "<li>Creation d'un site web en WordPress</li><li>Création d'une application mobile en Java avec l'IDE Android Studio</li><li>Gestion des réseaux sociaux de l'entreprise.</li>",
+  },
+  {
+    title: "",
+    cardImage: "",
+    place: "",
+    time: "",
+    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "GirlScript Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/1.jpg",
+    description:
+      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+  },
+  {
+    title: "StudentCode-in 2020",
+    cardImage: "assets/images/experience-page/2.jpg",
+    description:
+      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+  },
+  {
+    title: "PClub Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/3.jpg",
+    description:
+      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+  },
+  {
+    title: "Hakin-Codes",
+    cardImage: "assets/images/experience-page/4.jpg",
+    description:
+      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+  },
+];
+
